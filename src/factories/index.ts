142ee359bdb113bfe6f2387a942afb151b7391ca
Wilmer Lopez <wilc0519@gmail.com>
2023-11-29 13:43:47 -0500

    test: unit test for endpoint get all teams

---
+++ src/factories/index.ts
@@ -0,0 +1,5 @@
+import team from './team.factory';
+
+export default {
+  team,
+};
--- src/router/team.routes.ts
+++ src/router/team.routes.ts
@@ -1,8 +1,8 @@
-import Router from 'express'
-import { TeamController } from '../controllers/team.controller'
+import Router from 'express';
+import { TeamController } from '../controllers/team.controller';
 
-const router = Router()
+const router = Router();
 
-router.get('/', TeamController.getAll)
+router.get('/', TeamController.getAll);
 
-export default router
+export default router;
